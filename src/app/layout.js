79295d3cd33f3a735df25b33fa155e340937228f
Dpinajhh <davinfabian0906@outlook.com>
2026-08-05 10:07:38 +0700

    docs

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,6 +29,11 @@ export default function RootLayout({ children }) {
       className={`${poppins.variable} ${inter.variable} antialiased`}
     >
       <body>
+        <div className="fixed inset-0 -z-50 overflow-hidden pointer-events-none">
+          <div className="absolute top-[-10%] left-[-10%] w-[50vw] h-[50vw] rounded-full bg-[#2563EB]/[0.04] blur-[100px] animate-blob" />
+          <div className="absolute top-[30%] right-[-10%] w-[55vw] h-[55vw] rounded-full bg-[#EAB308]/[0.03] blur-[120px] animate-blob animation-delay-2000" />
+          <div className="absolute bottom-[-10%] left-[10%] w-[60vw] h-[60vw] rounded-full bg-[#38BDF8]/[0.04] blur-[100px] animate-blob animation-delay-4000" />
+        </div>
         <LenisProvider>{children}</LenisProvider>
       </body>
     </html>
